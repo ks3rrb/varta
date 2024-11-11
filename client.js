e094b449b99ddb433c6a5870e23f01570893165c
Rohit Rana <kshiva3.rrb@gmail.com>
2024-11-11 21:03:01 +0530

    correct url

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 //const { name } = require("ejs");
 
-const socket=io('http://localhost:8000');
+const socket=io('https://mzelo.com:8000');
 const form=document.getElementById('send-container');
 const messageInput=document.getElementById('messageInp');
 const messageContainer=document.querySelector('.main');
